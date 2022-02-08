03d30113944723ea8038fc5eaf6e19250561c704
Nicolas Blavet <245957@mail.muni.cz>
2022-02-08 10:32:25 +0000

    Upload New File

---
+++ final_report/script.py
@@ -0,0 +1,33 @@
+#############################################################
+# wrapper for rule: final_report
+#############################################################
+import os
+from snakemake.shell import shell
+shell.executable("/bin/bash")
+log_filename = str(snakemake.log)
+config_filename = str(snakemake.params.config)
+
+f = open(log_filename, 'a+')
+f.write("\n##\n## RULE: final_report \n##\n")
+f.close()
+
+command = " cp '"+os.path.abspath(os.path.dirname(__file__))+"/customer_report.Rmd' customer_report/"
+
+shell(command)
+
+command = """ Rscript -e "rmarkdown::render('customer_report/customer_report.Rmd', params=list(config = '""" + config_filename + """' ))" """ +\
+            " >> " + log_filename + " 2>&1 "
+
+f = open(log_filename, 'a+')
+f.write("## COMMAND: "+command+"\n")
+shell(command)
+
+command = " ls " + snakemake.output.html
+
+f = open(log_filename, 'a+')
+f.write("## COMMAND: "+command+"\n")
+shell(command)
+
+command = " rm customer_report/customer_report.Rmd"
+
+shell(command)
