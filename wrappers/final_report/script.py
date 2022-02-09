f6c20bb2e23995113e93266f588fa0e34eeb5356
Niko <245957@muni.cz>
2022-02-09 11:33:16 +0100

    update

--- wrappers/final_report/script.py
+++ wrappers/final_report/script.py
@@ -14,6 +14,13 @@ command = " cp '"+os.path.abspath(os.path.dirname(__file__))+"/customer_report.R
 
 shell(command)
 
+command = """ for i in results/DE_*/*_vs_*/*/*ggplot2.pdf results/DE_*/*_vs_*/*/*ggpubr.pdf results/DE_*/*_vs_*/*/heatmap*.pdf ; do convert -units PixelsPerInch -density 200 $i $i.png ; done"""
+
+f = open(log_filename, 'a+')
+f.write("## Converting pdf image in png.\n")
+f.write("## COMMAND: "+command+"\n")
+shell(command)
+
 command = """ Rscript -e "rmarkdown::render('customer_report/customer_report.Rmd', params=list(config = '""" + snakemake.params.config + """', set_author =  '""" + snakemake.params.author + """', set_email = '""" + snakemake.params.email + """'))" """ +\
             " >> " + log_filename + " 2>&1 "
 
